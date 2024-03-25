a16b7a70fc0169ea9ef4fb8766412e3e8635ed21
Rikard Johansson <rikjo@alvis2.int.private>
2024-03-25 20:33:29 +0100

    Implemented TETS, agent currently not learning well. Implemented epsilon greedy agent to compare to see which hyperparamter tuning that may be needed

--- agents.py
+++ agents.py
@@ -6,7 +6,6 @@ random.seed(42)
 import pandas as pd
 import numpy as np
 import xgboost as xgb
-from sklearn.model_selection import GridSearchCV,train_test_split
 from agent import Agent
 from utils.utils_xgboost import get_leafs
 
@@ -15,6 +14,7 @@ from utils.utils_xgboost import get_leafs
 
 class TETS(Agent):
     def __init__(self, use_cuda=False, epsilon=0.05,
+                 exploration_factor=1,
                 max_depth=6, n_estimators=100,
                 eta=0.3, gamma=0, xgb_lambda=1.0):
         print("initializing agent")
@@ -24,7 +24,9 @@ class TETS(Agent):
         self.observations = []
         self.rewards = []
         self.feature_types = ['q' for _ in range(1)]+['c' for _ in range(22)]
-
+        self.lr = eta
+        self.actions = []
+        self.exploration_factor = exploration_factor
         self.model_parameters = {'booster': 'gbtree', 'tree_method': 'hist', 
                                  'objective': 'reg:squarederror', 'max_depth': max_depth, 
                                  'gamma': gamma, 'learning_rate': eta, 
@@ -41,13 +43,17 @@ class TETS(Agent):
         start = time.time()
         Xy = xgb.DMatrix(X, y, feature_types=self.feature_types, enable_categorical=True)
         self.model = xgb.train(params = self.model_parameters, dtrain=Xy, num_boost_round=self.n_estimators)
-        end = time.time()
-        #print("model trained in {:.3} seconds".format(end-start))
-        leaf_scores = self.model.get_booster().get_dump(with_stats=True, dump_format='json')
+
+        leaf_scores = self.model.get_dump(with_stats=True, dump_format='json')
+        
+        #gets residuals for first tree which has constant prediction s
         residuals_array = np.array(y - 0.5, dtype="float64")
+        
         json_trees = [json.loads(leaf_scores[i]) for i in range(self.n_estimators)]
         self.leaves_per_tree = []
+        X = xgb.DMatrix(X, feature_types=self.feature_types, enable_categorical=True)
 
+        # Iterate through all individual trees in the sample and collect the leavs
         for idx, tree in enumerate(json_trees):
             #print('len(tree)', len(tree))
             if not self.leaves_per_tree:  #always append leafs of first tree (even if only one leaf)
@@ -56,22 +62,34 @@ class TETS(Agent):
                 break
             else:
                 self.leaves_per_tree.append(get_leafs(tree))
-                #print(get_leafs(tree)[1])#.__sizeof__())
-
-            pred = self.model.predict(self.x_train.T, iteration_range=(0,idx+1)).reshape((len(self.reward_theta),1))
-            #logging.info('Staged pred ' + str(idx) + ': ' + str(pred))
-            #logging.info('Target ' + str(idx) + ': ' + str(self.reward_theta))
-            residuals_array = np.append(residuals_array, self.reward_theta - pred, axis=1)
-        leaf_of_data = np.array(self.model.apply(self.x_train.T), dtype="int64")
+                """
+                try:
+                    print(get_leafs(tree).__sizeof__())
+                    print(get_leafs(tree)[1])#.__sizeof__())
+                except:
+                    print("Exception: get_leafs(tree)")
+                """
+            pred = self.model.predict(X, iteration_range=(0,idx+1)).reshape((len(self.rewards),1))
+            logging.info('Staged pred ' + str(idx) + ': ' + str(pred))
+            logging.info('Target ' + str(idx) + ': ' + str(self.rewards))
+            residuals_array = np.append(residuals_array, self.rewards - pred, axis=1)
+        
+        individual_preds = self.model.predict(X, pred_leaf=True)
+        leaf_of_data = np.array(individual_preds)
 
         for i in range(len(self.leaves_per_tree)):
             df2 = pd.DataFrame({'leaf':leaf_of_data[:,i], 'residual':residuals_array[:,i]})
             group2 = df2.groupby(by='leaf').var()
         
-        for row_idx, row in group2.iterrows():
-            self.leaves_per_tree[i][row_idx]["row_idx"] = row_idx
-            self.leaves_per_tree[i][row_idx]["leaf_variance"] = row['residual'] * self.lr**2
+            for row_idx, row in group2.iterrows():
+                self.leaves_per_tree[i][row_idx]["row_idx"] = row_idx
+                #print('row_idx:'+str(row_idx))
+                self.leaves_per_tree[i][row_idx]["leaf_variance"] = row['residual'] * self.lr**2
+                #print("leaf_variance:"+str(row['residual'] * self.lr**2))
 
+        end = time.time()
+        print("model trained in {:.3} seconds".format(end-start))
+            
     def update_observations(self, observation, action, reward):
         self.observations.append(observation)
         self.rewards.append(reward)
@@ -81,13 +99,80 @@ class TETS(Agent):
         self.t += 1
 
     def get_samples(self, X):
-        samples = self.model.predict(xgb.DMatrix(X))
+        samples = self.predict(xgb.DMatrix(X))
         return samples
+
+    def predict(self, X):
+        mu_hat = self.model.predict(X)
+        individual_preds = self.model.predict(X, pred_leaf=True)
+        leaf_assignments = np.array(individual_preds)
+        n_samples_total = len(self.rewards)
+        variance_of_mean_per_assigned_leaf = np.array([self.leaves_per_tree[i][leaf_assignments[0][i]]["leaf_variance"] / self.leaves_per_tree[i][leaf_assignments[0][i]]["cover"] for i in range(len(self.leaves_per_tree))])
+        total_variance_of_mean = np.sum(variance_of_mean_per_assigned_leaf)
+
+        return mu_hat + np.sqrt(total_variance_of_mean) * np.random.randn()     
 
     def pick_action(self, observations):
         arms, _ = observations.shape
-        if (self.t<10) or (random.uniform(0,1)<(1/self.t)):
+        if (self.t<10) or (random.uniform(0,1)<(1/self.t)): #initial random pulls
             return random.randint(0,arms-1)
-        else:
-            return np.random.choice(np.flatnonzero(self.get_samples(observations)==self.get_samples(observations).max()))
+        else: # sample rewards for all possible actions, np.random.choice break ties if more than one option is considered optimal
+            samples = self.get_samples(observations)
+            print(samples)
+            action = np.random.choice(np.flatnonzero(samples==samples.max()))
+            self.actions.append(action)
+            if self.t%9000==0:
+                print(self.actions)
+            return action
 
+class XGBGreedy(Agent):
+    def __init__(self, use_cuda=False, epsilon=0.05,
+                max_depth=6, n_estimators=100,
+                gamma=0, xgb_lambda=1.0, eta=0.3):
+        print("initializing agent")
+        self.t = 1
+        self.n_estimators = n_estimators
+        self.max_depth = max_depth
+        self.observations = []
+        self.rewards = []
+        self.feature_types = ['q' for _ in range(1)]+['c' for _ in range(22)]
+        self.actions = []
+        self.model_parameters = {'booster': 'gbtree', 'tree_method': 'hist', 
+                                 'objective': 'reg:squarederror', 'max_depth': max_depth, 
+                                 'gamma': gamma, 'learning_rate': eta, 
+                                 'reg_lambda': xgb_lambda, 'min_child_weight': 2}
+        Agent.__init__(self)
+    def __str__(self):
+        return "This is the XGBGreedy contextual multi-armed bandit"
+    
+    def train_model(self, observations, rewards):
+        X = np.array(observations)
+        y = np.array(rewards).reshape(len(rewards),1)
+        Xy = xgb.DMatrix(X, y, feature_types=self.feature_types, enable_categorical=True)
+        self.model = xgb.train(params = self.model_parameters, dtrain=Xy, num_boost_round=self.n_estimators)
+            
+    def update_observations(self, observation, action, reward):
+        self.observations.append(observation)
+        self.rewards.append(reward)
+        # add some if-statement to avoid training model at each iteration
+        if self.t==9 or self.t%100==0:
+            self.train_model(self.observations,self.rewards)
+        self.t += 1
+
+    def get_samples(self, X):
+        samples = self.predict(xgb.DMatrix(X))
+        return samples
+
+    def predict(self, X):
+        mu_hat = self.model.predict(X)
+        return mu_hat
+
+    def pick_action(self, observations):
+        arms, _ = observations.shape
+        if (self.t<10) or (random.uniform(0,1)<(1/self.t)): #initial random pulls
+            return random.randint(0,arms-1)
+        else: # sample rewards for all possible actions, np.random.choice break ties if more than one option is considered optimal
+            samples = self.get_samples(observations)
+            action = np.random.choice(np.flatnonzero(samples==samples.max()))
+            self.actions.append(action)
+            return action
--- main.py
+++ main.py
@@ -7,15 +7,23 @@ from xgboost import XGBRegressor
 from agents import TETS
 import matplotlib.pyplot as plt
 import argparse
+import logging
 
+#TODO make config handler
 parser = argparse.ArgumentParser()
 parser.add_argument('--result_path', help='result_path', type=str)
+parser.add_argument('--time_steps', help='time_steps', type=int)
 args, _ = parser.parse_known_args()
 
 if args.result_path is not None:
     result_path = args.result_path
+if args.time_steps is not None:
+    timesteps = args.time_steps
+else:
+    timesteps = 10000
 
-timesteps = 10000
+logging.info("Initializing experiment for " +str(timesteps) + " timesteps")
+
 X_history = []
 y_history = []
 y_optimal = []
@@ -23,49 +31,58 @@ regrets = []
 
 if __name__ == "__main__":
     #return movie_df, rating_df, user_df
-    df_movies, df_user_ratings, df_user_data = get_data()
-    sampled_users = df_user_data.sample(n=timesteps, random_state=1, replace=True)
-    model = TETS()
+    agentz = [TETS()]
+    for a in agentz:
+        df_movies, df_user_ratings, df_user_data = get_data()
+        sampled_users = df_user_data.sample(n=timesteps, random_state=1, replace=True)
+        model = a
 
-    for t in range(0,timesteps):
-        if t%10==0:
-            print("timestep: "+str(t))
-            print(df_user_ratings.describe())
-        # Get contextual data regarding the current user
-        current_user = sampled_users.iloc[t]
-        current_user_ratings = df_user_ratings.loc[df_user_ratings['user_id']==current_user.user_id]
-        current_user_rated_movies = df_movies[df_movies['movie_id'].isin(current_user_ratings.movie_id.values)]
-        user_features = current_user.drop(index=('user_id'))
-        # Get contextual data regarding all movies rated by current_user
-        action_features = current_user_rated_movies.drop(columns=['movie_title','release_date'])
-        # Gets all ratings which will serve as the ground truth when comparing the recommendations
-        ratings = current_user_ratings.rating.values
-        rows, columns = action_features.shape
+        for t in range(0,timesteps):
+            if t%100==0:
+                print("timestep: "+str(t))
+            # Get contextual data regarding the current user
+            while True:
+                current_user = sampled_users.iloc[t]
+                current_user_ratings = df_user_ratings.loc[df_user_ratings['user_id']==current_user.user_id]
+                current_user_rated_movies = df_movies[df_movies['movie_id'].isin(current_user_ratings.movie_id.values)]
 
-        # X presents the possible actions to the agent
-        X = np.concatenate((np.tile(user_features,(rows,1)),action_features), axis=1)
-        # y are the rewards for each actions, not presented to the agent.
-        y = ratings.reshape(rows,1)
+                if (len(current_user_rated_movies)<=1):
+                    #TODO drop the current user from sampled_users df
+                    logging.info("User has no more rated movies, sampling a new user")
+                    t = random.randint(0,len(df_user_data))
+                else:
+                    break
+            user_features = current_user.drop(index=('user_id'))
 
-        action = model.pick_action(observations=X)
-        model.update_observations(observation=X[action],action=action,reward=y[action])
+            # Get contextual data regarding all movies rated by current_user
+            action_features = current_user_rated_movies.drop(columns=['movie_title','release_date'])
+            # Gets all ratings which will serve as the ground truth when comparing the recommendations
+            ratings = current_user_ratings.rating.values
+            rows, columns = action_features.shape
+
+            # X presents the possible actions to the agent
+            X = np.concatenate((np.tile(user_features,(rows,1)),action_features), axis=1)
+            # y are the rewards for each actions, not presented to the agent.
+            y = ratings.reshape(rows,1)
+
+            action = model.pick_action(observations=X)
+            model.update_observations(observation=X[action],action=action,reward=y[action])
+
+            max_reward = ratings[ratings.argmax(axis=0)]
+            y_optimal.append(max_reward)
+            print("Recommended movie had rating {}, most liked movie had rating {}, regret for timestep {} is therefore {}".format(y[action],max_reward,t,max_reward-y[action]))
+            regret = get_regret(max_reward,y[action])
+            regrets.append(regret)
+
+            #remove the recommended movie, movie_id, from rated movies by user_id
+            recommended_movie_id = current_user_rated_movies.iloc[action].movie_id
+            df_user_ratings = df_user_ratings.loc[~((df_user_ratings['movie_id'] == recommended_movie_id) & (df_user_ratings['user_id']==current_user.user_id))] 
 
 
-        max_reward = ratings[ratings.argmax(axis=0)]
-        y_optimal.append(max_reward)
-        #print("Recommended movie had rating {}, most liked movie had rating {}, regret for timestep {} is therefore {}".format(reward,max_reward,t,max_reward-reward))
-        regret = get_regret(max_reward,y[action])
-        regrets.append(regret)
+        ts = range(0,timesteps)
+        plt.plot(ts,regrets,'.',linestyle='')
+        plt.savefig(result_path+'/instant_regret.png')
+        plt.clf()
 
-        #remove the recommended movie, movie_id, from rated movies by user_id
-        recommended_movie_id = current_user_rated_movies.iloc[action].movie_id
-        df_user_ratings = df_user_ratings.loc[~((df_user_ratings['movie_id'] == recommended_movie_id) & (df_user_ratings['user_id']==current_user.user_id))] 
-
-
-    ts = range(0,timesteps)
-    plt.plot(ts,regrets,'.',linestyle='')
-    plt.savefig(result_path+'/instant_regret.png')
-    plt.clf()
-
-    plt.plot(ts,np.cumsum(regrets))
-    plt.savefig(result_path+'/cum_regret.png')
+        plt.plot(ts,np.cumsum(regrets))
+        plt.savefig(result_path+'/cum_regret.png')
